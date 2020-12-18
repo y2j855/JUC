5c0891b2a95a3fc22363e4f59e9f63a76f94da17
y2j855 <y2j855@hotmail.com>
2020-12-18 12:19:53 +0800

    unsafe basic knowledge

--- src/main/java/com/tony/juc/c_012_02_Unsafe/HelloUnsafe.java
+++ src/main/java/com/tony/juc/c_012_02_Unsafe/HelloUnsafe.java
@@ -1,6 +1,29 @@
-package com.tony.juc.c_012_02_Unsafe;/**
-  * @author: Tony.Chen
-  * Create Time : 2020/12/18 09:21
-  * Description:
-  */  public class HelloUnsafe {
+package com.tony.juc.c_012_02_Unsafe;
+
+import sun.misc.Unsafe;
+
+/**
+ * @author: Tony.Chen
+ * Create Time : 2020/12/18 09:21
+ * Description: Unsafe类
+ * Unsafe相当于c，c++操作内存的能力
+ * 它能够对内存进行直接操作
+ * AtomicXXX都是使用Unsafe来实现CAS
+ * weakCompareAndSet/Swap()...
+ *
+ * jdk11之后将Unsafe关闭了，通过反射也不能使用了。
+ */
+public class HelloUnsafe {
+    static class M{
+        private M(){}
+        int i = 0;
+    }
+
+    public static void main(String[] args) throws InstantiationException {
+        Unsafe unsafe = Unsafe.getUnsafe();
+        M m = (M) unsafe.allocateInstance(M.class);
+        m.i = 9;
+        System.out.println(m.i);
+
+    }
 }

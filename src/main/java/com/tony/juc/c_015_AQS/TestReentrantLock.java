ac915c79f4497155ca78af1332e4d5650dc8ac0a
y2j855 <y2j855@hotmail.com>
2020-12-21 21:30:12 +0800

    add VarHandle usage

--- src/main/java/com/tony/juc/c_015_AQS/TestReentrantLock.java
+++ src/main/java/com/tony/juc/c_015_AQS/TestReentrantLock.java
@@ -1,6 +1,19 @@
-package com.tony.juc.c_015_AQS;/**
-  * @author: Tony.Chen
-  * Create Time : 2020/12/21 09:42
-  * Description:
-  */  public class TestReentrantLock {
+package com.tony.juc.c_015_AQS;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author: Tony.Chen
+ * Create Time : 2020/12/21 09:42
+ * Description: 学习AQS源码
+ */
+public class TestReentrantLock {
+    private static int i = 0;
+
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        i++;
+        lock.unlock();
+    }
 }
--- src/main/java/com/tony/juc/c_015_VarHandle/T01_HelloVarHandle.java
+++ src/main/java/com/tony/juc/c_015_VarHandle/T01_HelloVarHandle.java
@@ -1,6 +1,46 @@
-package com.tony.juc.c_015_VarHandle;/**
-  * @author: Tony.Chen
-  * Create Time : 2020/12/21 19:51
-  * Description:
-  */  public class T01_HelloVarHandle {
+package com.tony.juc.c_015_VarHandle;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+
+/**
+ * @author: Tony.Chen
+ * Create Time : 2020/12/21 19:51
+ * Description: VarHandle用法
+ * VarHandle是能够任何类型对象的引用，并且能够保证普通对象的原子操作。
+ * 它的性能比反射要高很多，本质就是直接操作二进制码
+ */
+public class T01_HelloVarHandle {
+    int x = 8;
+
+    private static VarHandle handle;
+
+    static{
+        try {
+            handle = MethodHandles.lookup().findVarHandle(T01_HelloVarHandle.class,"x",int.class);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        T01_HelloVarHandle t = new T01_HelloVarHandle();
+
+        /**
+         * read / write
+         * 能够获得对象的引用，并且它的操作是原子性
+         * 不用加锁就能保证原子性。
+         */
+        System.out.println(handle.get(t));
+        handle.set(t,9);
+        System.out.println(t.x);
+
+        handle.compareAndSet(t,9,10);
+        System.out.println(t.x);
+
+        handle.getAndAdd(t,10);
+        System.out.println(t.x);
+    }
 }

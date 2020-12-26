c48321fca20ca6bc91bf9901323c582a0bb0a0be
y2j855 <y2j855@hotmail.com>
2020-12-26 16:53:36 +0800

    add delayqueue run task by runtime

--- src/main/java/com/tony/juc/c_019_MapAndSet/T09_DelayQueue.java
+++ src/main/java/com/tony/juc/c_019_MapAndSet/T09_DelayQueue.java
@@ -1,6 +1,78 @@
-package com.tony.juc.c_019_MapAndSet;/**
-  * @author: Tony.Chen
-  * Create Time : 2020/12/26 16:42
-  * Description:
-  */  public class T09_DelayQueue {
+package com.tony.juc.c_019_MapAndSet;
+
+import com.tony.juc.c_016_RefTypeAndThreadLocal.M;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: Tony.Chen
+ * Create Time : 2020/12/26 16:42
+ * Description: 按照等待时间来执行队列中的任务
+ * 使用场景：按时间进行任务调度
+ * 自定的类需要继承Delayed对象，实现getDelay，compareTo方法
+ */
+public class T09_DelayQueue {
+    static BlockingQueue<MyTask> tasks = new DelayQueue<>();
+
+    static Random r = new Random();
+
+    static class MyTask implements Delayed {
+        String name;
+        long runningTime;
+
+        public MyTask(String name, long runningTime) {
+            this.name = name;
+            this.runningTime = runningTime;
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(runningTime - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        }
+
+        @Override
+        public int compareTo(Delayed o) {
+            if(this.getDelay(TimeUnit.MILLISECONDS) < o.getDelay(TimeUnit.MILLISECONDS)){
+                return -1;
+            }else if(this.getDelay(TimeUnit.MILLISECONDS) > o.getDelay(TimeUnit.MILLISECONDS)){
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "MyTask{" +
+                    "name='" + name + '\'' +
+                    ", runningTime=" + runningTime +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long now = System.currentTimeMillis();
+
+        MyTask t1 = new MyTask("t1",now + 1000);
+        MyTask t2 = new MyTask("t2",now + 2000);
+        MyTask t3 = new MyTask("t3",now + 1500);
+        MyTask t4 = new MyTask("t4",now + 2500);
+        MyTask t5 = new MyTask("t5",now + 500);
+
+        tasks.put(t1);
+        tasks.put(t2);
+        tasks.put(t3);
+        tasks.put(t4);
+        tasks.put(t5);
+
+        System.out.println(tasks);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(tasks.take());
+        }
+    }
 }
